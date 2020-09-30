17907761f7aec61781d9c23730b5ae26906b17c6
luzko <luzdim45@gmail.com>
2020-09-30 11:38:06 +0300

    refactored controller

--- src/main/java/com/luzko/libraryapp/controller/LibraryController.java
+++ src/main/java/com/luzko/libraryapp/controller/LibraryController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet(urlPatterns = {"/controller/*"})
 public class LibraryController extends HttpServlet {
@@ -32,16 +33,21 @@ public class LibraryController extends HttpServlet {
         processRequest(request, response);
     }
 
+    //TODO добавить объект, что не гонять request вне контроллера..
+
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String commandName = request.getParameter(RequestParameter.COMMAND_NAME);
-        Command command = ActionProvider.defineCommand(commandName);
-        //TODO Optional<Command> если существует, то норм. Если нет, то сразу на страницу ошибки.
-        //TODO добавить объект, что не гонять request вне контроллера..
-        Router router = command.execute(request, response);
-        if (router.getRouterType().equals(RouterType.FORWARD)) {
-            request.getRequestDispatcher(router.getPagePath()).forward(request, response);
+        Optional<Command> commandOptional = ActionProvider.defineCommand(commandName);
+        if(commandOptional.isPresent()) {
+            Router router = commandOptional.get().execute(request, response);
+            if (router.getRouterType().equals(RouterType.FORWARD)) {
+                request.getRequestDispatcher(router.getPagePath()).forward(request, response);
+            } else {
+                response.sendRedirect(router.getPagePath());
+            }
         } else {
-            response.sendRedirect(router.getPagePath());
+            //TODO данной команды не существет..
+            request.getRequestDispatcher(PagePath.ERROR).forward(request, response);
         }
     }
 
--- src/main/java/com/luzko/libraryapp/controller/command/ActionProvider.java
+++ src/main/java/com/luzko/libraryapp/controller/command/ActionProvider.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Optional;
+
 public class ActionProvider {
     private static final Logger logger = LogManager.getLogger(ActionProvider.class);
 
@@ -11,19 +13,16 @@ public class ActionProvider {
 
     }
 
-    public static Command defineCommand(String commandName) {
-        Command command = null;
-        if (commandName != null && !commandName.isBlank()) {
+    public static Optional<Command> defineCommand(String commandName) {
+        Optional<Command> command = Optional.empty();
+        if(commandName != null && !commandName.isBlank()) {
             try {
                 CommandType commandType = CommandType.valueOf(commandName.toUpperCase());
-                command = commandType.getCommand();
+                command = Optional.ofNullable(commandType.getCommand());
             } catch (IllegalArgumentException e) {
                 logger.log(Level.ERROR, "Incorrect command: {}", commandName, e);
             }
         }
-
         return command;
     }
 }
-
-//TODO public static Optional<Command> defineCommand(String commandName) {
