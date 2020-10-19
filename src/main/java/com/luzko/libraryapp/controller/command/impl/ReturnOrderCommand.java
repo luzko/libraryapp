74316fe834297c7c623c3f59887bf56e8c911a13
luzko <luzdim45@gmail.com>
2020-10-20 00:13:25 +0300

    refactored define order command

--- src/main/java/com/luzko/libraryapp/controller/command/impl/ReturnOrderCommand.java
+++ src/main/java/com/luzko/libraryapp/controller/command/impl/ReturnOrderCommand.java
@@ -28,16 +28,8 @@ public class ReturnOrderCommand implements Command {
         String bookId = request.getParameter(RequestParameter.BOOK_ID);
         try {
             if (orderService.isReturn(orderId, bookId)) {
-                Object userIdObject = request.getSession().getAttribute(RequestParameter.USER_ID);
-                long userId = (long) userIdObject;
-                String currentPageString = request.getParameter(RequestParameter.CURRENT_PAGE);
-                int currentPage = currentPageString != null ? Integer.parseInt(currentPageString) : 1;
-                int recordsPerPage = Integer.parseInt(RequestParameter.RECORD_PAGE);
-                List<Order> orders = orderService.findByUserId(userId);
-                definePagination(request, orders.size(), currentPage, recordsPerPage);
-                int recordsView = (currentPage - 1) * recordsPerPage;
-                orders = orders.subList(recordsView, Math.min(recordsView + recordsPerPage, orders.size()));
-                request.getSession().setAttribute(RequestParameter.ALL_ORDERS, orders);
+                List<Order> orderList = defineOrderList(orderService, request);
+                request.getSession().setAttribute(RequestParameter.ALL_ORDERS, orderList);
                 request.getSession().setAttribute(RequestParameter.ORDER_TYPE, orderType);
                 router.setPagePath(PagePath.ORDERS);
                 router.setRouterType(RouterType.REDIRECT);
@@ -51,5 +43,17 @@ public class ReturnOrderCommand implements Command {
             router.setRouterType(RouterType.FORWARD);
         }
         return router;
+    }
+
+    private List<Order> defineOrderList(OrderService orderService, HttpServletRequest request) throws ServiceException {
+        Object userIdObject = request.getSession().getAttribute(RequestParameter.USER_ID);
+        long userId = (long) userIdObject;
+        String currentPageString = request.getParameter(RequestParameter.CURRENT_PAGE);
+        int currentPage = currentPageString != null ? Integer.parseInt(currentPageString) : 1;
+        int recordsPerPage = Integer.parseInt(RequestParameter.RECORD_PAGE);
+        List<Order> orderList = orderService.findByUserId(userId);
+        definePagination(request, orderList.size(), currentPage, recordsPerPage);
+        int recordsView = (currentPage - 1) * recordsPerPage;
+        return orderList.subList(recordsView, Math.min(recordsView + recordsPerPage, orderList.size()));
     }
 }
