2d47abae803af2c7a68a4b0703486835c1dcaba7
luzko <luzdim45@gmail.com>
2020-10-27 16:12:02 +0300

    reafactored book dao

--- src/main/java/com/luzko/libraryapp/model/dao/impl/BookDaoImpl.java
+++ src/main/java/com/luzko/libraryapp/model/dao/impl/BookDaoImpl.java
@@ -119,32 +119,34 @@ public class BookDaoImpl implements BookDao {
     }
 
     private Optional<Book> createBook(ResultSet resultSet) throws SQLException {
-        BookBuilder bookBuilder = new BookBuilder()
-                .setBookId(resultSet.getLong(ColumnName.BOOK_ID))
-                .setTitle(resultSet.getString(ColumnName.TITLE))
-                .setYear(resultSet.getInt(ColumnName.YEAR))
-                .setPage(resultSet.getInt(ColumnName.PAGES))
-                .setDescription(resultSet.getString(ColumnName.DESCRIPTION))
-                .setNumberCopy(resultSet.getInt(ColumnName.NUMBER))
-                .setCategory(Category.defineCategoryById(resultSet.getInt(ColumnName.CATEGORY_ID_FK)))
-                .setAuthor(resultSet.getString(ColumnName.AUTHORS));
-        Book book = new Book(bookBuilder);
         Optional<Book> bookOptional = Optional.empty();
-        if (book.getCategory() != null) {
+        Optional<Category> categoryOptional = Category.defineCategoryById(resultSet.getInt(ColumnName.CATEGORY_ID_FK));
+        if (categoryOptional.isPresent()) {
+            BookBuilder bookBuilder = new BookBuilder()
+                    .setBookId(resultSet.getLong(ColumnName.BOOK_ID))
+                    .setTitle(resultSet.getString(ColumnName.TITLE))
+                    .setYear(resultSet.getInt(ColumnName.YEAR))
+                    .setPage(resultSet.getInt(ColumnName.PAGES))
+                    .setDescription(resultSet.getString(ColumnName.DESCRIPTION))
+                    .setNumberCopy(resultSet.getInt(ColumnName.NUMBER))
+                    .setCategory(categoryOptional.get())
+                    .setAuthor(resultSet.getString(ColumnName.AUTHORS));
+            Book book = new Book(bookBuilder);
             bookOptional = Optional.of(book);
         }
         return bookOptional;
     }
 
     private Optional<Book> createBookOverview(ResultSet resultSet) throws SQLException {
-        BookBuilder bookBuilder = new BookBuilder()
-                .setBookId(resultSet.getLong(ColumnName.BOOK_ID))
-                .setTitle(resultSet.getString(ColumnName.TITLE))
-                .setCategory(Category.defineCategoryById(resultSet.getInt(ColumnName.CATEGORY_ID_FK)))
-                .setAuthor(resultSet.getString(ColumnName.AUTHORS));
-        Book book = new Book(bookBuilder);
         Optional<Book> bookOptional = Optional.empty();
-        if (book.getCategory() != null) {
+        Optional<Category> categoryOptional = Category.defineCategoryById(resultSet.getInt(ColumnName.CATEGORY_ID_FK));
+        if (categoryOptional.isPresent()) {
+            BookBuilder bookBuilder = new BookBuilder()
+                    .setBookId(resultSet.getLong(ColumnName.BOOK_ID))
+                    .setTitle(resultSet.getString(ColumnName.TITLE))
+                    .setCategory(categoryOptional.get())
+                    .setAuthor(resultSet.getString(ColumnName.AUTHORS));
+            Book book = new Book(bookBuilder);
             bookOptional = Optional.of(book);
         }
         return bookOptional;
