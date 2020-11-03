bfb09698c8d0ffd961b0681071e975d305656db6
luzko <luzdim45@gmail.com>
2020-11-03 23:45:16 +0300

    refactored library page command

--- src/main/java/com/luzko/libraryapp/controller/command/impl/page/LibraryPageCommand.java
+++ src/main/java/com/luzko/libraryapp/controller/command/impl/page/LibraryPageCommand.java
@@ -1,7 +1,6 @@
 package com.luzko.libraryapp.controller.command.impl.page;
 
 import com.luzko.libraryapp.controller.AttributeName;
-import com.luzko.libraryapp.controller.AttributeValue;
 import com.luzko.libraryapp.controller.PagePath;
 import com.luzko.libraryapp.controller.RequestParameter;
 import com.luzko.libraryapp.controller.Router;
@@ -10,13 +9,11 @@ import com.luzko.libraryapp.exception.ServiceException;
 import com.luzko.libraryapp.model.entity.Book;
 import com.luzko.libraryapp.model.factory.ServiceFactory;
 import com.luzko.libraryapp.model.service.BookService;
-import com.luzko.libraryapp.util.ConfigurationManager;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
 import java.util.List;
 
 public class LibraryPageCommand implements Command {
@@ -27,11 +24,11 @@ public class LibraryPageCommand implements Command {
         Router router = new Router();
         Object searchName = request.getSession().getAttribute(RequestParameter.SEARCH);
         try {
-            if(searchName != null) {
-                findBook(request, searchName);
-            } else {
+            if (searchName == null) {
                 List<Book> bookList = defineBookList(request);
                 request.getSession().setAttribute(AttributeName.ALL_BOOKS, bookList);
+            } else {
+                findBook(request, searchName);
             }
             router.setPagePath(PagePath.LIBRARY);
             return router;
@@ -45,13 +42,7 @@ public class LibraryPageCommand implements Command {
     private void findBook(HttpServletRequest request, Object searchNameObject) throws ServiceException {
         String searchName = (String) searchNameObject;
         List<Book> bookList = defineSearchBookList(searchName, request);
-        if(bookList.isEmpty()) {
-            String attributeValue = ConfigurationManager.getMessageProperty(AttributeValue.PATH_BOOK_NOT_FOUND,
-                    (String) request.getSession().getAttribute(AttributeName.LOCALE));
-            request.setAttribute(AttributeName.NOT_FOUND_BOOKS, attributeValue);
-        } else {
-            request.getSession().setAttribute(AttributeName.ALL_BOOKS, bookList);
-        }
+        request.getSession().setAttribute(AttributeName.ALL_BOOKS, bookList);
     }
 
     private List<Book> defineBookList(HttpServletRequest request) throws ServiceException {
@@ -63,12 +54,8 @@ public class LibraryPageCommand implements Command {
 
     private List<Book> defineSearchBookList(String searchName, HttpServletRequest request) throws ServiceException {
         BookService bookService = ServiceFactory.getInstance().getBookService();
-        List<Book> bookList = Collections.emptyList();
         int countRecords = bookService.findCountRecords(searchName);
-        if (countRecords > 0) {
-            int shownRecords = shownRecordsPagination(countRecords, request);
-            bookList = bookService.findByName(searchName, shownRecords, RECORDS_PER_PAGE);
-        }
-        return bookList;
+        int shownRecords = shownRecordsPagination(countRecords, request);
+        return bookService.findByName(searchName, shownRecords, RECORDS_PER_PAGE);
     }
 }
