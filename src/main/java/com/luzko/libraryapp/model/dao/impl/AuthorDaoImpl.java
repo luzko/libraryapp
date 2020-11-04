a8862263ef8d3da15a59bf8ce50ef1113c434bcb
luzko <luzdim45@gmail.com>
2020-11-05 00:26:37 +0300

    refactored dao

--- src/main/java/com/luzko/libraryapp/model/dao/impl/AuthorDaoImpl.java
+++ src/main/java/com/luzko/libraryapp/model/dao/impl/AuthorDaoImpl.java
@@ -1,10 +1,10 @@
 package com.luzko.libraryapp.model.dao.impl;
 
-import com.luzko.libraryapp.model.connection.ConnectionPool;
 import com.luzko.libraryapp.exception.DaoException;
+import com.luzko.libraryapp.model.connection.ConnectionPool;
+import com.luzko.libraryapp.model.dao.AuthorDao;
 import com.luzko.libraryapp.model.dao.ColumnName;
 import com.luzko.libraryapp.model.dao.StatementSql;
-import com.luzko.libraryapp.model.dao.AuthorDao;
 import com.luzko.libraryapp.model.entity.Author;
 
 import java.sql.Connection;
